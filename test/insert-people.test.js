6703968489019d0157750ddea9ebc62119ed232b
Bruna Pereira <bpereira@thoughtworks.com>
2018-01-30 17:18:50 -0200

    removes database population logic

--- test/insert-people.test.js
+++
@@ -1,9 +0,0 @@
-const insertPeople = require('../src/insert-people')
-
-describe('insertPeople', () => {
-  it('prepares person to insert')
-
-  it('insert one person')
-
-  it('insert all people')
-})
